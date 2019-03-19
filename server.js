fc3013b0944cf2c52148593d02050558d1255705
Trung <trung@Trungs-MacBook-Pro.local>
2019-03-19 11:00:43 +0700

    fix link

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 async function pull_ov() {
-  const { stdout, stderr } = await exec('cd /home/trung/apps/client && git pull && npm install && npm run build && cd /home/trung/apps/server && git pull');
+  const { stdout, stderr } = await exec('cd /home/trung/apps/client && git pull && npm install && npm run build && cd /home/trung/apps/server && git pull && npm install');
   console.log('out:', stdout);
   console.log('err:', stderr);
 }
